8710b045e550eb8a25038ed87d603aad248f7d15
Jongseok Choi <jseokchoi@smilegate.com>
2017-05-24 12:47:27 +0900

    asfd

---
+++ demo/test.js
@@ -0,0 +1,4 @@
+function load() {
+    Tag1 = '<li class="video loaded"><a href="http://player.vimeo.com/video/8974462?byline=0&amp;portrait=0" rel="video"><img src="http://b.vimeocdn.com/ts/432/699/43269900_100.jpg" title="Victorias Secret"></a></li>'
+    document.getElementById("gallery").innerHTML += Tag1
+}
